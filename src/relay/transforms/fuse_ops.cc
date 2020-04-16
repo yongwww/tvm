f72acf2f53ce72cad6862a7fa0d86558fea7c0a1
Yong Wu <ywu118@alumni.jh.edu>
2020-04-17 00:32:01 +0800

    fix for fuse

--- src/relay/transforms/fuse_ops.cc
+++ src/relay/transforms/fuse_ops.cc
@@ -250,6 +250,10 @@ class IndexedForwardGraph::Creator : private ExprVisitor {
     if (call->attrs.as<StridedSliceAttrs>()) {
       bool is_dyn{false};
       for (auto arg :  call->args) {
+        if (!arg.as<ConstantNode>()) {
+           is_dyn = true;
+           break;
+        }
         auto arg_tt = arg->checked_type().as<TensorTypeNode>();
         if (arg_tt) {
           for (auto dim : arg_tt->shape) {
@@ -264,6 +268,7 @@ class IndexedForwardGraph::Creator : private ExprVisitor {
         op_pattern = kInjective;
       }
     }
+
     node->pattern = op_pattern;
     this->Update(call->op, nullptr, kOpaque);
     const auto* rtype = call->checked_type().as<TensorTypeNode>();
