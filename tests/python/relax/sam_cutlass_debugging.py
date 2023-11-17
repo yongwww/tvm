f6a598968978e21f1cff11b17568443b668d45f2
Yong Wu <yongcale@gmail.com>
2023-11-16 16:57:16 -0800

    Apply Wuwei's change

--- tests/python/relax/sam_cutlass_debugging.py
+++ tests/python/relax/sam_cutlass_debugging.py
@@ -10,35 +10,6 @@ import tvm.topi.testing
 from tvm.relax.backend.contrib.cutlass import partition_for_cutlass
 
 
-def run_opt_passes(mod, params=None, fp16_input_names=None, combine_matmul=False):
-    passes = [
-        relax.transform.EliminateCommonSubexpr(),
-        relax.transform.CanonicalizeBindings(),
-        # relax.transform.ConvertLayout({"relax.nn.conv2d": ["NHWC", "OHWI"]}),
-        # get_rewrite_pass(combine_matmul)
-        relax.transform.DeadCodeElimination(["main"]),
-    ]
-
-    if params:
-        passes += [
-            relax.transform.BindParams("main", params),
-            relax.transform.FoldConstant(),
-            relax.transform.ToMixedPrecision(out_dtype="float16"),
-        ]
-    else:
-        passes += [
-            relax.transform.FoldConstant(),
-            # relax.transform.ToMixedPrecision(
-            #     out_dtype="float16", fp16_input_names=fp16_input_names
-            # ),
-        ]
-        """
-              File "/home/ubuntu/tvm/src/relax/transform/infer_amp_utils.cc", line 28
-              InternalError: Check failed: (tensor) is false: Expected TensorStructInfo, but got R.Objec
-        """,
-
-    return tvm.transform.Sequential(passes)(mod)
-
 
 def offload_to_cutlass(
     mod, target, entry_functions=["main", "get_prompt_embeddings", "get_image_embeddings"]
@@ -83,157 +54,22 @@ def run_lower_passes(mod, target, do_tuning=True):
         return tvm.transform.Sequential(passes)(mod)
 
 
-metadata = tvm.ir.load_json(
-    """{
-  \"root\": 1, 
-  \"nodes\": [
-    {
-      \"type_key\": \"\"
-    }, 
-    {
-      \"type_key\": \"Map\", 
-      \"keys\": [
-        \"relax.expr.Constant\"
-      ], 
-      \"data\": [2]
-    }, 
-    {
-      \"type_key\": \"Array\", 
-      \"data\": [3]
-    }, 
-    {
-      \"type_key\": \"relax.expr.Constant\", 
-      \"attrs\": {
-        \"_checked_type_\": \"13\", 
-        \"data\": \"0\", 
-        \"span\": \"0\", 
-        \"struct_info_\": \"4\"
-      }
-    }, 
-    {
-      \"type_key\": \"relax.TensorStructInfo\", 
-      \"attrs\": {
-        \"dtype\": \"float32\", 
-        \"ndim\": \"4\", 
-        \"shape\": \"5\", 
-        \"span\": \"0\", 
-        \"vdevice\": \"0\"
-      }
-    }, 
-    {
-      \"type_key\": \"relax.expr.ShapeExpr\", 
-      \"attrs\": {
-        \"_checked_type_\": \"12\", 
-        \"span\": \"0\", 
-        \"struct_info_\": \"11\", 
-        \"values\": \"6\"
-      }
-    }, 
-    {
-      \"type_key\": \"Array\", 
-      \"data\": [7, 8, 9, 10]
-    }, 
-    {
-      \"type_key\": \"IntImm\", 
-      \"attrs\": {
-        \"dtype\": \"int64\", 
-        \"span\": \"0\", 
-        \"value\": \"1\"
-      }
-    }, 
-    {
-      \"type_key\": \"IntImm\", 
-      \"attrs\": {
-        \"dtype\": \"int64\", 
-        \"span\": \"0\", 
-        \"value\": \"1\"
-      }
-    }, 
-    {
-      \"type_key\": \"IntImm\", 
-      \"attrs\": {
-        \"dtype\": \"int64\", 
-        \"span\": \"0\", 
-        \"value\": \"1\"
-      }
-    }, 
-    {
-      \"type_key\": \"IntImm\", 
-      \"attrs\": {
-        \"dtype\": \"int64\", 
-        \"span\": \"0\", 
-        \"value\": \"2\"
-      }
-    }, 
-    {
-      \"type_key\": \"relax.ShapeStructInfo\", 
-      \"attrs\": {
-        \"ndim\": \"4\", 
-        \"span\": \"0\", 
-        \"values\": \"6\"
-      }
-    }, 
-    {
-      \"type_key\": \"relax.ShapeType\", 
-      \"attrs\": {
-        \"ndim\": \"4\", 
-        \"span\": \"0\"
-      }
-    }, 
-    {
-      \"type_key\": \"relax.DynTensorType\", 
-      \"attrs\": {
-        \"dtype\": \"float32\", 
-        \"ndim\": \"4\", 
-        \"span\": \"0\"
-      }
-    }
-  ], 
-  \"b64ndarrays\": [
-    \"P6G0lvBAXt0AAAAAAAAAAAEAAAAAAAAABAAAAAIgAQABAAAAAAAAAAEAAAAAAAAAAQAAAAAAAAACAAAAAAAAAAgAAAAAAAAAAAAAAAAAAAA=\"
-  ], 
-  \"attrs\": {\"tvm_version\": \"0.15.dev0\"}
-}"""
-)
-
 
 @I.ir_module
 class Module:
     @R.function
     def main(
-        input_points: R.Tensor((1, 1, 1, 2), dtype="float32"),
+        input_points: R.Tensor((1, 1, 2, 2), dtype="float32"),
         prompt_encoder_shared_embedding_positional_embedding: R.Tensor((2, 128), dtype="float32"),
     ):  # -> R.Tuple(R.Tensor((1, 1, 2, 128), dtype="float32"), R.Tensor((1, 1, 2, 128), dtype="float32")):
-        R.func_attr({"num_input": 3})
+        R.func_attr({"num_input": 2})
         with R.dataflow():
-            add255: R.Tensor((1, 1, 1, 2), dtype="float32") = R.add(
-                input_points, R.const(0.5, "float32")
-            )
-            concat6: R.Tensor((1, 1, 2, 2), dtype="float32") = R.concat(
-                (add255, metadata["relax.expr.Constant"][0]), axis=2
-            )
-            strided_slice18: R.Tensor((1, 1, 2, 1), dtype="float32") = R.strided_slice(
-                concat6, axes=[3], begin=[0], end=[1], strides=None, assume_inbound=False
-            )
-            divide8: R.Tensor((1, 1, 2, 1), dtype="float32") = R.divide(
-                strided_slice18, R.const(1024, "float32")
-            )
-            strided_slice19: R.Tensor((1, 1, 2, 1), dtype="float32") = R.strided_slice(
-                concat6, axes=[3], begin=[1], end=[2], strides=None, assume_inbound=False
-            )
-            divide9: R.Tensor((1, 1, 2, 1), dtype="float32") = R.divide(
-                strided_slice19, R.const(1024, "float32")
-            )
-            concat8: R.Tensor((1, 1, 2, 2), dtype="float32") = R.concat((divide8, divide9), axis=3)
-            add256: R.Tensor((1, 1, 2, 2), dtype="float32") = R.add(concat8, concat8)
-            subtract56: R.Tensor((1, 1, 2, 2), dtype="float32") = R.subtract(
-                add256, R.const(1, "float32")
-            )
             matmul146: R.Tensor((1, 1, 2, 128), dtype="float32") = R.matmul(
-                subtract56, prompt_encoder_shared_embedding_positional_embedding, out_dtype="void"
+                input_points, prompt_encoder_shared_embedding_positional_embedding, out_dtype="void"
             )
             mul128: R.Tensor((1, 1, 2, 128), dtype="float32") = R.multiply(
                 R.const(6.2831854820251465, "float32"), matmul146
+                # R.const(1., "float32"), matmul146
             )
             debugging: R.Tensor((1, 1, 2, 128), dtype="float32") = R.cos(mul128)
             R.output(matmul146, debugging)
@@ -241,7 +77,8 @@ class Module:
 
 
 def get_random_inputs(max_val=1):
-    input_points = tvm.nd.array(np.random.rand(1, 1, 1, 2).astype(np.float32), tvm.gpu())
+    np.random.seed(1)
+    input_points = tvm.nd.array(np.random.rand(1, 1, 2, 2).astype(np.float32), tvm.gpu())
     embedding = tvm.nd.array(
         np.random.uniform(low=-max_val, high=max_val, size=(2, 128)).astype(np.float32), tvm.gpu()
     )
@@ -256,8 +93,6 @@ def test_matmul_cutlass(inputs=None, apply_cutlass=True):
 
     mod = Module
 
-    mod = run_opt_passes(mod, combine_matmul=False)
-    # print(mod.script(show_meta=True))
     if apply_cutlass:
         mod = offload_to_cutlass(mod, target, ["main"])
     else:
@@ -279,12 +114,16 @@ def test_matmul_cutlass(inputs=None, apply_cutlass=True):
 
 if __name__ == "__main__":
     # inputs = get_random_inputs(max_val=1)
-    inputs = get_random_inputs(max_val=1000)
+    inputs = get_random_inputs(max_val=1)
 
     out2 = test_matmul_cutlass(inputs, apply_cutlass=False)
     out1 = test_matmul_cutlass(inputs, apply_cutlass=True)
 
-    tvm.testing.assert_allclose(out1[0].asnumpy(), out2[0].asnumpy(), rtol=1e-1, atol=1e-1)
-    tvm.testing.assert_allclose(out1[1].asnumpy(), out2[1].asnumpy(), rtol=1e-1, atol=1e-1)
+    if isinstance(out1, tvm.container.Array):
+        for i, (o1, o2) in enumerate(zip(out1, out2)):
+            print('Comparing output ', i)
+            tvm.testing.assert_allclose(o1.numpy(), o2.numpy(), rtol=1e-1, atol=1e-1)
+    else:
+        tvm.testing.assert_allclose(out1.numpy(), out2.numpy(), rtol=1e-1, atol=1e-1)
 
     print("Inference results match!")
