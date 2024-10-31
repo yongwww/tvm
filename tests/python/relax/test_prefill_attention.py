7a2bdc9fa4aba5b2f3f3147e443e9c58b5b4d87a
Yong Wu <yongcale@gmail.com>
2024-10-31 17:13:53 +0000

    add attention test via vm

--- tests/python/relax/test_prefill_attention.py
+++ tests/python/relax/test_prefill_attention.py
@@ -1,7 +1,9 @@
-# pylint: disable=line-too-long,missing-docstring
 import time
 from typing import Dict, List, Tuple, Union
 import numpy as np
+import torch
+import flashinfer
+import argparse
 import tvm
 from tvm import tir, relax
 from tvm.relax.frontend.nn import core, modules, spec
@@ -14,19 +16,16 @@ from tvm.runtime import ShapeTuple
 import mlc_llm
 from mlc_llm.compiler_pass import pipeline
 from mlc_llm.nn.kv_cache import PagedKVCache, RopeMode
-# mypy: disable-error-code="attr-defined"
-# pylint: disable=invalid-name,unused-argument,too-many-locals,too-many-statements
 
 """
 paged_kv_cache: R.Object
-t0: R.Tensor((seq_len, 48/tp, 128), dtype="float16") 
+t0: R.Tensor((seq_len, 48/tp, 128), dtype="float16")
 a0 = R.call_dps_packed("vm.builtin.attention_kv_cache_attention_with_fused_qkv", (paged_kv_cache, R.prim_value(0), R.prim_value(T.float32(1.0)), t0), out_sinfo=R.Tensor((seq_len, 32/tp, 128), dtype="float16"))
 a1 = R.call_dps_packed("vm.builtin.attention_kv_cache_attention_with_fused_qkv", (paged_kv_cache, R.prim_value(1), R.prim_value(T.float32(1.0)), t0), out_sinfo=R.Tensor((seq_len, 32/tp, 128), dtype="float16"))
 a2 = R.call_dps_packed("vm.builtin.attention_kv_cache_attention_with_fused_qkv", (paged_kv_cache, R.prim_value(2), R.prim_value(T.float32(1.0)), t0), out_sinfo=R.Tensor((seq_len, 32/tp, 128), dtype="float16"))
 ...
 a31 = R.call_dps_packed("vm.builtin.attention_kv_cache_attention_with_fused_qkv", (paged_kv_cache, R.prim_value(31), R.prim_value(T.float32(1.0)), t0), out_sinfo=R.Tensor((seq_len, 32/tp, 128), dtype="float16"))
 """
-
 
 ##########
 reserved_nseq = 32
@@ -407,8 +406,8 @@ def test_nn_module_paged_kv_cache():
             support_sliding_window_1 = T.int64()
             R.func_attr({"num_input": 5})
 
-                #$ paged_kv_cache: R.Object = R.call_pure_packed("mlc.create_paged_kv_cache_generic", R.shape([max_batch_size, max_total_seq_len, prefill_chunk_size, page_size, support_sliding_window]         ), R.shape([0, 32]), R.prim_value(32), R.prim_value(32), R.prim_value(8), R.prim_value(128), R.prim_value(1), R.prim_value(1), R.prim_value(T.float32(500000.0)), R.str("{}"), R.prim_value(0), R.prim_value(128), R.dtype("float16"), sinfo_args=(R.Object,))
-            
+            #$ paged_kv_cache: R.Object = R.call_pure_packed("mlc.create_paged_kv_cache_generic", R.shape([max_batch_size, max_total_seq_len, prefill_chunk_size, page_size, support_sliding_window]         ), R.shape([0, 32]), R.prim_value(32), R.prim_value(32), R.prim_value(8), R.prim_value(128), R.prim_value(1), R.prim_value(1), R.prim_value(T.float32(500000.0)), R.str("{}"), R.prim_value(0), R.prim_value(128), R.dtype("float16"), sinfo_args=(R.Object,))
+
             with R.dataflow():
                 paged_kv_cache: R.Object = R.call_pure_packed("mlc.create_paged_kv_cache_generic", R.shape([max_batch_size_1, max_total_seq_len_1, prefill_chunk_size_1, page_size_1, support_sliding_window_1]), R.shape([0, 32]), R.prim_value(32), R.prim_value(32), R.prim_value(32), R.prim_value(128), R.prim_value(1), R.prim_value(1), R.prim_value(10000),  R.str("{}"), R.prim_value(0), R.prim_value(128), R.dtype("float16"), sinfo_args=(R.Object,))
                 gv1: R.Object = paged_kv_cache
@@ -511,39 +510,6 @@ def test_nn_module_paged_kv_cache():
     vm = relax.VirtualMachine(ex, dev)
 
     # inference
-    """
-    max_batch_size=max_batch_size,
-                max_total_seq_len=max_total_seq_len,
-                prefill_chunk_size=prefill_chunk_size,
-                page_size=page_size,
-                support_sliding_window=support_sliding_window,
-                num_hidden_layers=32,
-                num_attention_heads=32,
-                num_key_value_heads=32,
-                head_dim=128,
-                rope_mode=RopeMode.NORMAL,
-                rope_scale=1,
-                rope_theta=10000,
-                rotary_dim=128,
-                dtype="float16"
-                rx.ShapeExpr(
-                    [
-                        max_batch_size,
-                        max_total_seq_len,
-                        prefill_chunk_size,
-                        page_size,
-                        support_sliding_window,
-                    ]
-                ),
-                rx.ShapeExpr(layer_partition),
-                rx.PrimValue(num_hidden_layers),
-                rx.PrimValue(num_attention_heads),
-                rx.PrimValue(num_key_value_heads),
-                rx.PrimValue(head_dim),
-                rx.PrimValue(rope_mode),
-                rx.PrimValue(rope_scale),
-                rx.PrimValue(rope_theta),
-    """
     max_batch_size = tvm.nd.array(1, dev)
 
     # cache = vm['create_flashinfer_paged_kv_cache'](tvm.runtime.ShapeTuple([10]), tvm.runtime.ShapeTuple([2048]), tvm.runtime.ShapeTuple([512]), tvm.runtime.ShapeTuple([16]), tvm.runtime.ShapeTuple([1]))
@@ -666,12 +632,12 @@ def test_prefill_attention():
         res = vm['forward'](cache, nd_qkv)
         cuda.cudart.cudaProfilerStop()
         print("profiled")
-        return 
-    
+        return
+
     # with relax vm
     for _ in range(warmup):
        res = vm['forward'](cache, nd_qkv)
-    
+
     start_time = time.time_ns()
     for _ in range(iterations):
         res = vm['forward'](cache, nd_qkv)
@@ -693,6 +659,78 @@ def test_prefill_attention():
     # res = vm['forward'](cache, tvm.nd.array(np.random.rand(1, 100, 48, 128).astype(np.float16), dev))
 
 
+def bench_flashinfer_batch_prefill_with_paged_kv_cache(
+    batch_size=7,
+    num_layers=32,
+    num_qo_heads=32,
+    num_kv_heads=8,
+    head_dim=128,
+    page_size=16,
+    nnz_qo=100,
+    kv_len=54,
+    kv_layout="NHD",
+    tensor_parallel_shards=1,
+):
+    num_qo_heads //= tensor_parallel_shards
+    num_kv_heads //= tensor_parallel_shards
+    # Allocate 128MB workspace buffer
+    workspace_buffer = torch.empty(128 * 1024 * 1024, dtype=torch.uint8, device="cuda:0")
+    prefill_wrapper = flashinfer.BatchPrefillWithPagedKVCacheWrapper(
+        workspace_buffer, kv_layout
+    )
+    num_pages_per_seq = (kv_len + page_size - 1) // page_size
+    total_num_pages = num_pages_per_seq * batch_size
+    qo_indptr = torch.arange(0, batch_size + 1, device="cuda:0", dtype=torch.int32)
+    kv_indptr = torch.arange(0, batch_size + 1, device="cuda:0", dtype=torch.int32) * num_pages_per_seq
+    kv_indices = torch.arange(0, total_num_pages, device="cuda:0", dtype=torch.int32)
+    kv_last_page_len = torch.full(
+        (batch_size,), (kv_len - 1) % page_size + 1, dtype=torch.int32, device="cuda:0"
+    )
+
+    q_at_layer = torch.randn(num_layers, nnz_qo, num_qo_heads, head_dim, dtype=torch.float16, device="cuda:0")
+    kv_cache_at_layer = torch.randn(
+        num_layers, total_num_pages, 2, page_size, num_kv_heads, head_dim, dtype=torch.float16, device="cuda:0"
+    )
+
+    # Create auxiliary data structures for batch prefill attention
+    prefill_wrapper.plan(
+        qo_indptr,
+        kv_indptr,
+        kv_indices,
+        kv_last_page_len,
+        num_qo_heads,
+        num_kv_heads,
+        head_dim,
+        page_size,
+        causal=True,
+    )
+
+    outputs = []
+    iterations = 100000
+    start_time = time.monotonic_ns()
+
+    for i in range(num_layers):
+        q = q_at_layer[i]
+        kv_data = kv_cache_at_layer[i]
+        for _ in range(iterations):
+            o = prefill_wrapper.run(q, kv_data)
+        outputs.append(o)
+
+    dur = (time.monotonic_ns() - start_time) / iterations / num_layers / 1000
+    print(f"Average flashinfer prefill attention with paged kv_cache took: {dur:.2f} μs")
+
+
 if __name__ == "__main__":
-    # test_nn_module_paged_kv_cache()
+    parser = argparse.ArgumentParser(description="Benchmark FlashInfer Batch Prefill with Paged KV Cache")
+    parser.add_argument("--batch_size", type=int, default=7, help="Batch size for the benchmark")
+    parser.add_argument("--tensor_parallel_shards", type=int, default=1, help="Number of tensor parallel shards")
+    parser.add_argument("--seq_len", type=int, default=300, help="The sequence length")
+
+    args = parser.parse_args()
     test_prefill_attention()
+
+    # bench_flashinfer_batch_prefill_with_paged_kv_cache(
+    #    batch_size=args.batch_size,
+    #    tensor_parallel_shards=args.tensor_parallel_shards,
+    #    nnz_qo=args.seq_len,
+    #    kv_len=args.seq_len)
